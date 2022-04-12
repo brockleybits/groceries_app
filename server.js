c1a88868c2744b9a0803ac2933a668144cc940da
brockleybits <todd.brotze@gmail.com>
2022-04-12 15:00:18 -0700

    Fixed typo in server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use('/login', require('./routes/login'));
 
 // Serve static assets if in PRODUCTION
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(Express.static('client/build'));
     app.get('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
